2695fc166ef2c7bae213f36ecd3d45b7d818a19f
jimmy <rootkaliente@gmail.com>
2023-09-13 09:39:42 -0500

    Prueba de cambiar textos

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 $hora = date("H:i:s");
 
 $title = $_GET['title'];
+$description = $_GET['description'];
+$image = $_GET['image'];
 
 // Generamos el código HTML de la página
 echo <<<HTML
@@ -11,6 +13,8 @@ echo <<<HTML
 <html lang="es">
 <head>
   <title>$title</title>
+  <meta name="description" content="$description" />
+  <meta property="og:image" content="$image">
 </head>
 <body>
   <h1>La hora actual es:</h1>
